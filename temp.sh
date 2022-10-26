5ecf98855da1f8238990156452c13a2660725782
ketankolte7 <49066365+ketankolte7@users.noreply.github.com>
2022-10-26 14:24:33 +0530

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install update
+sudo apt-get install default-jdk
+java -version
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hadoopusr
+sudo adduser hadoopusr sudo
+sudo apt-get install openssh-server
+su - hadoopusr
