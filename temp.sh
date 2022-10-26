a206c6ce77ca1ffd4e90de106cb4af8d9b23841f
ketankolte7 <49066365+ketankolte7@users.noreply.github.com>
2022-10-26 14:31:30 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -7,3 +7,11 @@ sudo adduser --ingroup hadoop hadoopusr
 sudo adduser hadoopusr sudo
 sudo apt-get install openssh-server
 su - hadoopusr
+ssh-keygen -t rsa -P ""
+cat $HOME/ .ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+ssh localhost
+wget https://archive.apache.org/dist/hadoop/common/hadoop-2.9.0/hadoop-2.9.0.tar.gz
+sudo tar xvzf hadoop-2.9.0.tar.gz
+mv hadoop-2.9.0 hadoop
+sudo mv hadoop /usr/local/hadoop
+sudo chown -R hadoopusr /usr/local
